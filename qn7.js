5b299552c390344555367f62f931c3c089901963
sanjay1013 <115240026+sanjay1013@users.noreply.github.com>
2022-11-06 21:08:37 +0530

    Add files via upload

---
+++ qn7.js
@@ -0,0 +1,18 @@
+let people = ['Greg','Mary' , 'Devon' , 'James']
+for(i = 0 ; i<people.length ; i++)
+console.log(people[i])
+people.shift();
+people.pop();
+people.unshift("Matt");
+people.push("SANJAY KUMAR");
+for(let i = 0; i < 2; i++){
+    console.log(people[i])
+  }
+let slicer = people.slice(2);
+console.log(slicer)
+console.log(people.indexOf("Mary"))
+console.log(people.indexOf("foo"))
+people.splice(2, 1, "elizabeth", "artie")
+console.log(people)
+let withBob=people.concat("bob")
+console.log(withBob)
