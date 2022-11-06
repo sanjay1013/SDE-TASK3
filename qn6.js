d0f1ce75251f2e7c9fb6e00cbc0a3486c71803e5
sanjay1013 <115240026+sanjay1013@users.noreply.github.com>
2022-11-06 22:04:29 +0530

    Update qn6.js

--- qn6.js
+++ qn6.js
@@ -1,11 +1,13 @@
-function tab(d){
-    table.push(d);
-}
-var table=[];
-var element1= ['name:"hydrogen"', 'H', 1.008];
-var element2= ['helium', 'He', 4.003];
-var element26 = ['iron', 'Fe', 55.85];
-tab(element1);
-tab(element2);
-tab(element26);
-console.log(table);
+element1 = ['hydrogen' , 'H' , 1.008]
+element2 = ['helium','He' ,4.003]
+element26 = ['iron' , 'Fe' , 55.85]
+table = []
+table.push(element1)
+table.push(element2)
+table.push(element26)
+console.log(table)
+console.log(table[1])
+console.log(table[1][1])
+console.log(table[0][2])
+console.log(table[1][0])
+console.log(table[2][1])
